f2af382a46e116c6f94dfcf8590c3abfc53a423b
Steven DeVere <stevendeverehere@gmail.com>
2022-01-19 22:17:43 -0500

    Adds styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React, { useState } from "react"
+import styled from "@emotion/styled"
 import { cipher } from "../helpers"
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  border: 12px black solid;
+  border-radius: 24px;
+  margin: 48px auto;
+  max-width: 1280px;
+`
+
+const Label = styled('label')`
+  display: block;
+  margin-bottom: 4px;
+`
+
+const MessageList = styled('div')`
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  grid-auto-rows: 200px;
+  width: 100%;
+`
+
+const MessageItem = styled('div')`
+  text-align: center;
+`;
 
 const IndexPage = () => {
   const [input, setInput] = useState("");
@@ -19,37 +47,59 @@ const IndexPage = () => {
   };
 
   return (
-    <div>
-      <input 
-        value={input}
-        placeholder="Input"
-        type="text"
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <input 
-        value={key}
-        placeholder="Key"
-        type="text"
-        onChange={(e) => setKey(e.target.value)}
-      />
+    <Container>
+      <h2>Vigenere Cipher</h2>
       <div>
-        <input type="radio" value="encrypt" name="action" checked={action === "encrypt"} onChange={() => setAction("encrypt")} /> Encrypt
-        <input type="radio" value="decrypt" name="action" checked={action === "decrypt"} onChange={() => setAction("decrypt")} /> Decrypt
+        <Label>Input:</Label>
+        <input 
+          value={input}
+          placeholder="Input"
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div>
+        <Label>Key:</Label>
+        <input
+          value={key}
+          placeholder="Key"
+          type="text"
+          onChange={(e) => setKey(e.target.value)}
+        />
+      </div>
+      <div>
+        <input 
+          type="radio"
+          value="encrypt"
+          name="action"
+          checked={action === "encrypt"}
+          onChange={() => setAction("encrypt")}
+        /> 
+        <span>Encrypt</span>
+        <input 
+          type="radio"
+          value="decrypt"
+          name="action" 
+          checked={action === "decrypt"}
+          onChange={() => setAction("decrypt")} 
+        />
+        <span>Decrypt</span>
       </div>
       <button onClick={onSubmit}>Submit</button>
-      {results.map(({ input, key, action, result }) => {
-        return (
-          <div key={`${input}_${key}_${action}`}>
-            <div>Input: {input}</div>
-            <div>Key: {key}</div>
-            <div>Action: {action}</div>
-            <div>Result: {result}</div>
-          </div>
-        )
-      })}
-    </div>
+      <MessageList>
+        {results.map(({ input, key, action, result }) => {
+          return (
+            <MessageItem key={`${input}_${key}_${action}`}>
+              <div>Input: {input}</div>
+              <div>Key: {key}</div>
+              <div>Action: {action}</div>
+              <div>Result: {result}</div>
+            </MessageItem>
+          )
+        })}
+      </MessageList>
+    </Container>
   )
-  
 }
 
 export default IndexPage
